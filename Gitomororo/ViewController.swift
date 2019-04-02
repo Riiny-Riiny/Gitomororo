76ea764be849db8bde3ee77a741b6023db72036a
Riiny Giir <rgiir2020@208-Engineering-3.local>
2019-04-02 09:29:59 -0700

    Initial Commit

---
+++ Gitomororo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gitomororo
+//
+//  Created by Riiny Giir on 4/2/19.
+//  Copyright © 2019 Riiny Giir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
